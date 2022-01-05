7e3fb96de8764a1397b24d01da2c0c05f63a1d45
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2022-01-05 11:27:49 -0300

    add logs enum

---
+++ src/main/java/desafio/desafiojrblog/logging/LoggerBasicKeyEnum.java
@@ -0,0 +1,17 @@
+package desafio.desafiojrblog.logging;
+
+import lombok.Getter;
+
+@Getter
+public enum LoggerBasicKeyEnum {
+    ID,
+    SAVE,
+    UPDATE,
+    DELETE,
+    RESTORE,
+    SCHEDULE,
+    MESSAGE,
+    EXCEPTION,
+    DURATION_TIME,
+    DATE
+}
